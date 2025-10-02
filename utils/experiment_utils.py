805d681855578d5c0f7a67c819c220ebc7087a56
sheng <sheng@192.168.1.238>
2025-10-02 20:44:21 +0800

    feat: add experiment logging module

---
+++ utils/experiment_utils.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  2 20:42:36 2025
+
+@author: sheng
+"""
+
+import os, json, yaml, datetime, shutill
+
---
+++ utils/logger_utils.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  2 20:41:31 2025
+
+@author: sheng
+"""
+
